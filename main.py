7877e6a63a60ca8a88fc50146da8778c22dca113
callum54 <164798486+callum54@users.noreply.github.com>
2024-05-14 11:28:06 +1200

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 
-
-# asks for users name
-name = input("what is your name? ")
-
 # asking if the user wants to see the instructions
-answer = input(f"Do you want to read the instructions {name}? ")
+answer = input(f"Do you want to read the instructions? ")
 if answer == "yes":
     print("a")
 elif answer == "no":
@@ -13,6 +9,6 @@ else:
     print("please answer with yes or no")
 
 # asks user how many questions they will get
-number_of_questions = int(input(f"how many questions would you like {name}? "))
+number_of_questions = int(input(f"how many questions would you like? "))
 print(f"you picked {number_of_questions}")
 
