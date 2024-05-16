25dd3b0fbe8074c2c6fc994d84339c4e67bd7d88
callum54 <164798486+callum54@users.noreply.github.com>
2024-05-16 14:59:52 +1200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import random
 
-
 # asking if the player wants to see the instructions.
+
+
 def yes_no(question):
     while True:
         answer = input(question).lower()
@@ -13,6 +14,8 @@ def yes_no(question):
             print("Please answer with yes or no")
 
 # instructions.
+
+
 def instructions():
     print('''
     
@@ -27,6 +30,8 @@ def instructions():
         good luck!
           ''')
 
+
+# game title
 print()
 print("➖basic facts maths game➕")
 print()
@@ -39,14 +44,87 @@ if want_instructions == "yes":
 
 # asks player how many questions they will get.
 number_of_questions = int(input(f"how many questions would you like? "))
-print(f"you will get {number_of_questions} questions on your test ")
+print(f"you will get {number_of_questions} questions on your quiz ")
 print()
-print("press enter to continue ")
+
+# difficulty levels
+
+
+def easy():
+    print("starting easy mode")
+    print()
+    # gets two random numbers
+    number_1 = random.randint(1, 50)
+    number_2 = random.randint(1, 50)
+
+    answer_1 = number_1 + number_2
+
+    # makes math equation and checks the players answer
+    user_answer = int(input(f" {number_1} + {number_2} "))
+    if user_answer == answer_1:
+        print("correct!")
+    else:
+        print("that one was not correct")
+        print(f"the correct answer was {answer_1}")
+
+
+def normal():
+    print("starting normal mode")
+    print()
+    # gets two random numbers
+    number_1 = random.randint(1, 100)
+    number_2 = random.randint(1, 100)
+
+    answer_1 = number_1 + number_2
+
+    # makes math equation and checks the players answer
+    user_answer = int(input(f" {number_1} + {number_2} "))
+    if user_answer == answer_1:
+        print("correct!")
+    else:
+        print("that one was not correct")
+        print(f"the correct answer was {answer_1}")
+
+
+
+def hard():
+    print("starting hard mode")
+    print()
+    # gets two random numbers
+    number_1 = random.randint(1, 160)
+    number_2 = random.randint(1, 160)
+
+    answer_1 = number_1 + number_2
+
+    # makes math equation and checks the players answer
+    user_answer = int(input(f" {number_1} + {number_2} "))
+    if user_answer == answer_1:
+        print("correct!")
+    else:
+        print("that one was not correct")
+        print(f"the correct answer was {answer_1}")
+
+
+def difficulty(question):
+    while True:
+        answer = input(question).lower()
+        if answer == "easy" or answer == "Easy":
+            return "easy"
+        elif answer == "normal" or answer == "Normal":
+            return "normal"
+        elif answer == "hard" or answer == "Hard":
+            return "hard"
+        else:
+            print("please type in either easy, normal or hard")
+
+
+want_difficulty = difficulty("select your difficulty of easy, normal or hard: ")
+
+print()
+print("press enter to start your quiz ")
 input()
 
-# gets two random numbers
-number = random.randint(1,100)
-number_2 = random.randint(1,100)
-
-print(f" {number} + {number_2} ")
-
+if want_difficulty == "easy":
+    easy()
+elif want_difficulty == "normal":
+    normal()
